2f8e4552cefd526b1593146a617db978f7b0ec71
Zamoula <jamelmrad000@gmail.com>
2024-07-10 23:21:39 +0100

    v1.1

--- src/main/java/jamel/projects/image_to_text/controller/OCRController.java
+++ src/main/java/jamel/projects/image_to_text/controller/OCRController.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api/ocr")
 public class OCRController {
 
