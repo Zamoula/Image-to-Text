3608bfa6956442de6b8f9209e8f7bd69dd108bb9
Zamoula <jamelmrad000@gmail.com>
2024-07-11 12:36:29 +0100

    V1.0

---
+++ script.js
@@ -0,0 +1,38 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click' , (e) => {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+
+    if (!file) {
+        alert('Please select a file.');
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    fetch('https://image-to-text-gzq3.onrender.com/api/ocr', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        },
+        body: formData
+    })
+    .then(response => {
+        if (!response.ok) {
+            return response.text().then(text => { throw new Error(text) });
+        }
+        const t = document.getElementById('txt');
+        t.innerHTML(response);
+        return response.json();
+    })
+    .then(data => {
+        console.log('Success:', data);
+        alert('File uploaded successfully!');
+    })
+    .catch(error => {
+        console.error('Error:', error.message);
+        alert('File upload failed!');
+    });
+});
